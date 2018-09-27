9133c19eaa354df841319747f3c8aa741dc00a49
Drazen Lucic <lucicd@hotmail.com>
2018-09-27 15:04:13 +0400

    Add PORT from environment

--- app.js
+++ app.js
@@ -9,35 +9,20 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 //use middleware
+app.use(express.static(path.join(__dirname, 
+  'bower_components')));
 
-app.use(bodyParser());
-
-var todoItems = [
-  {id: 1, desc: 'foo'},
-  {id: 2, desc: 'bar'},
-  {id: 3, desc: 'baz'},
-];
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 //define routes
 
-app.get('/', function(req, res) {
-  res.render('index', {
-    title: 'My App',
-    items: todoItems,
-  });
-});
+app.use(require('./todos'));
 
-app.post('/add', function(req, res) {
-  var newItem = req.body.newItem;
-  
-  todoItems.push({
-    id: todoItems.length + 1,
-    desc: newItem,
-  });
-  
-  res.redirect('/');
-});
+//start the server
 
-app.listen(1337, function() {
-  console.log('Ready on port 1337.');
+var port = process.env.PORT || 1337;
+
+app.listen(port, function() {
+  console.log('Ready on port ' + port);
 });
